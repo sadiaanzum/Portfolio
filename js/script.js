ece415e624bed9a45f7acb13cefe6bb0f880f1cf
Sadia Anzum <sadiaanzum@Sadias-MacBook-Air-2.local>
2024-03-31 22:13:01 -0400

    Final

--- js/script.js
+++ js/script.js
@@ -67,6 +67,17 @@ let darkModeIcon = document.querySelector('#darkMode-icon');
 darkModeIcon.onclick = () => {
   darkModeIcon.classList.toggle('bx-sun');
   document.body.classList.toggle('dark-mode');
-
 };
 
+/*-------SCROLL REVEAL--------*/
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration:2000,
+  delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.skills-container, .projects-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img img, .home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
